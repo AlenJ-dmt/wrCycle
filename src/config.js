cfcfe7fa29ebc75a22c0fed49559c272ce5b7fcc
Alen Diaz <noricual@gmail.com>
2021-04-15 16:58:48 -0500

    url fixed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://radiant-beach-27622.herokuapp.com/',
+    API_ENDPOINT: 'https://radiant-beach-27622.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
